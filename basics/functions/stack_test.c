ab9c528fc0ae2ce96370f0c02492a88a58a002ec
Alexander <agrayrussia@proton.me>
2025-02-28 14:13:19 +0300

    Finished functions_notes.md with gdb debugging an function basics

---
+++ basics/functions/stack_test.c
@@ -0,0 +1,17 @@
+/*
+This is an example program used to practice GDB debugging and execution modification in gdb.
+See "functions_notes.md" for more details
+*/
+
+#include <stdio.h>
+
+void say_hello(){
+    printf("Привет!");
+}
+
+int main(){
+    say_hello();
+    return 0;
+}
+
+
